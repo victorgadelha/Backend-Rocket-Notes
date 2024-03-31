601435e3bb6b17655b05fbda9a01448f64a6674b
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-03-31 18:24:12 -0300

    Fix: corrigindo middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
 
-const usersRoute = require("./users.routes");
+const usersRouter = require("./users.routes");
 
 const routes = Router();
 
-routes.use("/users, usersRouter");
+routes.use("/users", usersRouter);
 
 module.exports = routes;
